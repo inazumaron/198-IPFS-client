fcaddf8c8eb71c30e497d9d69f59843da8e9b5d0
JustroX <thepiesaresquared@gmail.com>
2022-01-09 01:26:39 +0800

    refactor: ui improvements on send

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,14 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { NgZorroAntdModule, NZ_I18N, en_US, NzIconModule } from "ng-zorro-antd";
+import {
+  NgZorroAntdModule,
+  NZ_I18N,
+  en_US,
+  NzIconModule,
+  NzResultModule,
+  NzAlertModule,
+} from "ng-zorro-antd";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -71,6 +78,8 @@ registerLocaleData(en);
     NzTypographyModule,
     NzCardModule,
     NzToolTipModule,
+    NzResultModule,
+    NzAlertModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
--- src/app/pages/post-send/post-send.component.ts
+++ src/app/pages/post-send/post-send.component.ts
@@ -1,58 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { ApiService } from "src/app/services/api.service";
 import { NzNotificationService } from "ng-zorro-antd";
 
 @Component({
-  selector: 'app-post-send',
-  templateUrl: './post-send.component.html',
-  styleUrls: ['./post-send.component.css']
+  selector: "app-post-send",
+  templateUrl: "./post-send.component.html",
+  styleUrls: ["./post-send.component.css"],
 })
 export class PostSendComponent implements OnInit {
-
-  CID = "TharanakyarehyephnilghringyaCthulhuvulgtlaglnmgngephriiTsathogguaeezhroNyarlathotepebunmahlirgh";
-  Passcode = "Password_1";
+  CID = "";
+  Passcode = "";
+  isCopied = false;
 
   constructor(
     private api: ApiService,
-    private route:ActivatedRoute,
+    private route: ActivatedRoute,
     private notification: NzNotificationService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.CID = this.route.snapshot.paramMap.get('cid');
-    this.Passcode = this.route.snapshot.paramMap.get('pc');
+    this.CID = this.route.snapshot.paramMap.get("cid");
+    this.Passcode = this.route.snapshot.paramMap.get("pc");
   }
 
   private copyOld(text: string) {
-    const textArea = document.createElement('textarea');
+    const textArea = document.createElement("textarea");
     textArea.value = text;
-    textArea.style.top = '0';
-    textArea.style.left = '0';
-    textArea.style.position = 'fixed';
-    textArea.style.display = 'none';
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+    textArea.style.display = "none";
     document.body.appendChild(textArea);
     textArea.focus();
     textArea.select();
-    const successful = document.execCommand('copy');
+    const successful = document.execCommand("copy");
     document.body.removeChild(textArea);
   }
 
   async copy(text: string) {
     if (!navigator.clipboard) {
       this.copyOld(text);
+      this.isCopied = true;
       return;
     }
     await navigator.clipboard.writeText(text);
+    this.isCopied = true;
   }
 
-  private async pin () {
+  async pin() {
     try {
-      this.api.pin(this.CID);
+      await this.api.pin(this.CID);
+      this.notification.success(
+        "Pin Queued",
+        "File now in pin queue. Please wait while it's being processed"
+      );
     } catch (err) {
       console.error(err);
       this.notification.error("Pin Failed", "Something went wrong.");
     }
   }
-
 }
